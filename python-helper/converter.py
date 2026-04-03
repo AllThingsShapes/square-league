2b43201d050521984aa909d393041433fda90bf7
Andrew Olson <bouncingballs247@gmail.com>
2026-04-03 14:28:09 -0500

    Updating website

--- python-helper/converter.py
+++ python-helper/converter.py
@@ -8,8 +8,11 @@ id = 1
 with open(file_path, 'r') as file, open(output_file_path, 'w') as output_file:
     for line in file:
         split_line = line.split('\t')
-        
-        stat_str = '{ ' + f'id: {id}, color: \"{split_line[0]}\", name: \"{split_line[1]}\", wins: {split_line[2]}, losses: {split_line[3]}, draws: {split_line[4]}, power_ups: {split_line[5]}, damage_done: {split_line[6]}, wall_bounces: {split_line[7]}, image: \"{split_line[8].replace('\n', '')}\"' + ' },'
+
+        wld = split_line[8]
+        wld_array = '[' + ', '.join(f'"{char}"' for char in wld) + ']'
+
+        stat_str = '{ ' + f'id: {id}, color: \"{split_line[0]}\", name: \"{split_line[1]}\", wins: {split_line[2]}, losses: {split_line[3]}, draws: {split_line[4]}, power_ups: {split_line[5]}, damage_done: {split_line[6]}, wall_bounces: {split_line[7]}, last5: {wld_array}, image: \"{split_line[9].replace('\n', '')}\"' + ' },'
         output_file.write(stat_str + '\n')
         id += 1
         
